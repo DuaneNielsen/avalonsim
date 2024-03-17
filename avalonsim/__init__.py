100aa14868950b9baeeabb39b4f642fc2539f93f
duanenielsen <duane.nielsen.rocks@gmail.com>
2024-03-17 10:06:46 -0700

    Update default settings.

--- avalonsim/__init__.py
+++ avalonsim/__init__.py
@@ -1,16 +1,18 @@
 from .env import Env, Weapon, Agent, Direction, FaceDirection, RangeFinder, CollisionLayer, Action, Wall
 from .env import close, between, overlap
-from gym.envs.registration import register, EnvSpec
+from gymnasium.envs.registration import register
+
 
 sword = Weapon(damage=8, shot_speed=1.0, time_to_live=0.01, shot_width=0.01, windup_time=0.05,
                recovery_time=0.04)
-bow = Weapon(damage=8, shot_speed=0.3, time_to_live=2., windup_time=2.0)
-player = Agent(pos=0.1, facing=Direction.EAST, collision_layer=CollisionLayer.PLAYER,
+bow = Weapon(damage=12, shot_speed=0.3, time_to_live=2., windup_time=1.8)
+player = Agent(pos=0.1, facing=Direction.EAST, walk_speed=0.05,
+               collision_layer=CollisionLayer.PLAYER,
                shot_collision_layer=CollisionLayer.PLAYER_SHOTS)
 player.weapon = sword
 player.add_vertex(RangeFinder(player.weapon.range))
 player.add_vertex(RangeFinder(-player.weapon.range))
-enemy = Agent(pos=0.9, facing=Direction.WEST)
+enemy = Agent(pos=0.9, facing=Direction.WEST, walk_speed=0.05)
 enemy.weapon = bow
 enemy.add_vertex(RangeFinder(enemy.weapon.range))
 enemy.add_vertex(RangeFinder(-enemy.weapon.range))
@@ -19,7 +21,7 @@ map = [player, enemy]
 register(
     id='Avalon-v1',
     entry_point='avalonsim.env:Env',
-    max_episode_steps=2000,
+    max_episode_steps=1000,
     reward_threshold=1.,
     kwargs={'map': map}
 )
